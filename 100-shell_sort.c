770c36050bbecc56d73ef48cd562fc9bc7c2a04c
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2022-10-11 02:53:39 +0200

    begin

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,60 +1,27 @@
 #include "sort.h"
 
 /**
- * swapme - swap the nodes themselves.
- * @current: pointer.
- * @current_old: pointer.
- * @list: doubly linked list
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: array to sort
+ * @size: size of array
  */
-void swapme(listint_t *current, listint_t *current_old, listint_t **list)
+void shell_sort(int *array, size_t size)
 {
-	listint_t *temp1 = current->next;
-	listint_t *temp2 = current_old->prev;
+	int gap = 1, i, j, tmp;
 
-	if (temp1 != NULL)
-		temp1->prev = current_old;
-	if (temp2 != NULL)
-		temp2->next = current;
-	current->prev = temp2;
-	current_old->next = temp1;
-	current->next = current_old;
-	current_old->prev = current;
-	if (*list == current_old)
-		*list = current;
-	print_list(*list);
-}
-
-/**
- * cocktail_sort_list - cocktail_sort_list
- *
- * @list: doubly linked list
- */
-void cocktail_sort_list(listint_t **list)
-{
-	listint_t *check = *list, *first = NULL, *last = NULL;
-
-	if (!list)
-		return;
-	if (!(*list))
-		return;
-	if (!(*list)->next)
-		return;
-	do {
-		while (check->next)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
+	{
+		for (i = gap; i < (int)size; i += 1)
 		{
-			if (check->n > check->next->n)
-				swapme(check->next, check, list);
-			else
-				check = check->next;
+			tmp = array[i];
+
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = tmp;
 		}
-		last = check;
-		while (check->prev != first)
-		{
-			if (check->n < check->prev->n)
-				swapme(check, check->prev, list);
-			else
-				check = check->prev;
-		}
-		first = check;
-	} while (first != last);
+		print_array(array, size);
+	}
 }
