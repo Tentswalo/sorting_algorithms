d3260cef29d7c42aef1718e3a30cc5cb29fa6c71
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2022-10-11 02:49:51 +0200

    begin

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,73 +1,60 @@
 #include "sort.h"
 
 /**
-* swapem - Swaps tha nodes
-* @l: left or lower node
-* @r: right or later node
-* @h: Head of dlist
-*/
-
-void swapem(listint_t *l, listint_t *r, listint_t **h)
+ * swapme - swap the nodes themselves.
+ * @current: pointer.
+ * @current_old: pointer.
+ * @list: doubly linked list
+ */
+void swapme(listint_t *current, listint_t *current_old, listint_t **list)
 {
-	listint_t *temp;
+	listint_t *temp1 = current->next;
+	listint_t *temp2 = current_old->prev;
 
-	temp = l->prev;
-	if (temp)
-		temp->next = r;
-	r->prev = temp;
-	l->prev = r;
-	l->next = r->next;
-	r->next = l;
-	if (l->next != NULL)
-		l->next->prev = l;
-	if (r->prev == NULL)
-		*h = r;
-	print_list(*h);
+	if (temp1 != NULL)
+		temp1->prev = current_old;
+	if (temp2 != NULL)
+		temp2->next = current;
+	current->prev = temp2;
+	current_old->next = temp1;
+	current->next = current_old;
+	current_old->prev = current;
+	if (*list == current_old)
+		*list = current;
+	print_list(*list);
 }
 
 /**
-* cocktail_sort_list - sorts dlist using cocktail sort
-* @list: Head to dlist
-*/
-
+ * cocktail_sort_list - cocktail_sort_list
+ *
+ * @list: doubly linked list
+ */
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *temp;
-	int isS = 1;
-	int start = 0, end = 0, c;
+	listint_t *check = *list, *first = NULL, *last = NULL;
 
-	if (list == NULL)
+	if (!list)
 		return;
-	temp = *list;
-	while (temp->next != NULL)
-		temp = temp->next, end++;
-	temp = *list;
-	while (isS == 1)
-	{
-		c = start;
-		while (c < end)
+	if (!(*list))
+		return;
+	if (!(*list)->next)
+		return;
+	do {
+		while (check->next)
 		{
-			if (temp->n > temp->next->n)
-			{
-				swapem(temp, temp->next, list);
-				isS = 1;
-			}
+			if (check->n > check->next->n)
+				swapme(check->next, check, list);
 			else
-				temp = temp->next;
-			c++;
+				check = check->next;
 		}
-		c = end, isS = 0;
-		while (c > start)
+		last = check;
+		while (check->prev != first)
 		{
-			if (temp->n < temp->prev->n)
-			{
-				swapem(temp->prev, temp, list);
-				isS = 1;
-			}
+			if (check->n < check->prev->n)
+				swapme(check, check->prev, list);
 			else
-				temp = temp->prev;
-			c--;
+				check = check->prev;
 		}
-		start++;
-	}
+		first = check;
+	} while (first != last);
 }
