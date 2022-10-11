099a1aadc436bc98fcb9c5c321dbb5796590e97b
Tentswalo <104768179+Tentswalo@users.noreply.github.com>
2022-10-11 02:45:39 +0200

    begin

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,90 +1,47 @@
 #include "sort.h"
+
 /**
-*swap - swaps 2 nodes in a doubly-linked list
-*@a: address of first node
-*@b: address of second node
-*
-*Return: void
-*/
-void swap(listint_t *a, listint_t *b)
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
 {
-	if (a->prev)
-		a->prev->next = b;
-	if (b->next)
-		b->next->prev = a;
-	a->next = b->next;
-	b->prev = a->prev;
-	a->prev = b;
-	b->next = a;
-}
-/**
-*tail_traverse- function that sorts from the tail back
-*
-*@head: head of list
-*@tail: tail of the list
-*@list: original head of the list
-*
-*Return: new head of the list
-*/
-listint_t *tail_traverse(listint_t *head, listint_t *tail, listint_t *list)
-{
-	while (tail && tail->prev)
-	{
-		if (tail->n < tail->prev->n)
-		{
-			swap(tail->prev, tail);
-			if (tail->prev == NULL)
-				list = tail;
-			print_list(list);
-		}
-		else
-			tail = tail->prev;
-		if (tail->prev == NULL)
-			head = tail;
-	}
-	return (head);
+	size_t n;
+
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
 
 /**
-*cocktail_sort_list - sorts linked list using cocktail shaker sort
-*
-*@list: doubly linked list to be sorted
-*/
-void cocktail_sort_list(listint_t **list)
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
+ */
+void shell_sort(int *array, size_t size)
 {
-	listint_t *tail, *head, *len;
-	int i = 0, j = 0, swaped = 1;
+	size_t gap, i, j;
+	int temp;
 
-	if (!list || !*list)
+	if (!array || !size)
 		return;
-	len = *list;
-	for (i = 0; len; i++)
+
+
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		len = len->next;
-	}
-	if (i < 2)
-		return;
-	head = *list;
-	while (j < i)
-	{
-		swaped = 0;
-		while (head && head->next)
+		for (i = gap; i < size; i++)
 		{
-			if (head->n > head->next->n)
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
 			{
-				swap(head, head->next);
-				swaped++;
-				if (head->prev->prev == NULL)
-					*list = head->prev;
-				print_list(*list);
+				array[j] = array[j - gap];
 			}
-			else
-				head = head->next;
-			if (head->next == NULL)
-				tail = head;
+			array[j] = temp;
 		}
-		head = tail_traverse(head, tail, *list);
-		*list = head;
-		j++;
+		print_array(array, size);
 	}
-}
